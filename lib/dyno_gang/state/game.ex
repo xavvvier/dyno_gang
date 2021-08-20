b7624d7f14b2feecf60a57e30a9acd9fd21572fc
xavvvier <xavvvier@users.noreply.github.com>
2021-08-19 21:01:25 -0300

    stop obstacle generator message if there is no players

--- lib/dyno_gang/state/game.ex
+++ lib/dyno_gang/state/game.ex
@@ -1,10 +1,14 @@
 defmodule DynoGang.State.Game do
   @derive {Jason.Encoder, only: [:players, :max_score]}
-  defstruct players: %{}, max_score: nil
+  defstruct players: %{}, max_score: nil, obstacle_generator: nil
 
   alias DynoGang.State.Game
 
   def player_names(%Game{}=game) do
     Enum.map(game.players, fn {_k,v} -> v.name end)
+  end
+
+  def total_players(%Game{}=game) do
+    Enum.count(game.players)
   end
 end
--- lib/dyno_gang_web/game_server.ex
+++ lib/dyno_gang_web/game_server.ex
@@ -4,7 +4,7 @@ defmodule DynoGangWeb.GameServer do
   alias DynoGang.State.{Game, Player, Score}
   alias DynoGangWeb.Endpoint
 
-  #Client
+  # Client
 
   def start_link(_) do
     GenServer.start(__MODULE__, %Game{}, name: :game_server)
@@ -30,61 +30,78 @@ defmodule DynoGangWeb.GameServer do
     GenServer.call(:game_server, {:player_names})
   end
 
-  #Server
+  # Server
 
   def init(state) do
-    :timer.send_after(3000, :obstable_generator)
     IO.puts("Game server started")
     {:ok, read_score(state)}
   end
 
   defp read_score(state) do
-    #Read the maximum score
+    # Read the maximum score
     {:ok, table} = :dets.open_file(:scoring, [])
     content = :dets.lookup(table, :max)
     :dets.close(table)
-    max_score = case content do
-      [max: [name_as_list, value]] ->
-        name = List.to_string(name_as_list)
-        %Score{username: name, value: value}
-      _ -> nil
-    end
-    %{state| max_score: max_score}
+
+    max_score =
+      case content do
+        [max: [name_as_list, value]] ->
+          name = List.to_string(name_as_list)
+          %Score{username: name, value: value}
+
+        _ ->
+          nil
+      end
+
+    %{state | max_score: max_score}
   end
 
   def handle_call({:player_join, player, character}, _from, state) do
     current_players = state.players
-    #create a new player state
+    obstacle_generator_ref = state.obstacle_generator
+
+    if is_nil(obstacle_generator_ref) do
+      {:ok, obstacle_generator_ref} = :timer.send_after(3000, :obstacle_generator)
+    end
+
+    # create a new player state
     IO.inspect(player, label: "adding player")
     player_state = %Player{name: player, character: character}
-    new_state = %{state |
-      players: Map.put(state.players, player, player_state)
+
+    new_state = %{
+      state
+      | players: Map.put(state.players, player, player_state),
+        obstacle_generator: obstacle_generator_ref
     }
+
     Endpoint.broadcast!("obstacle:all", "player_joined", player_state)
     {:reply, current_players, new_state}
   end
 
   def handle_call({:player_move, player_name, key, x, score}, _from, state) do
-    #get the player state
+    # get the player state
     player_state = Map.get(state.players, player_name)
-    #update the game state
-    state = %{state |
-      players: Map.put(state.players, player_name,
-        Player.move(player_state, key, x, score))
+    # update the game state
+    state = %{
+      state
+      | players: Map.put(state.players, player_name, Player.move(player_state, key, x, score))
     }
+
     {:reply, state, state}
   end
 
   def handle_call({:die, player_name}, _from, state) do
-    #get the player state
+    # get the player state
     player_state = Map.get(state.players, player_name)
-    user_score= %Score{username: player_name, value: player_state.score}
-    #update the game state
-    state = %{state |
-      players: Map.put(state.players, player_name, Player.die(player_state)) ,
-      max_score: Score.max(state.max_score, user_score)
+    user_score = %Score{username: player_name, value: player_state.score}
+    # update the game state
+    state = %{
+      state
+      | players: Map.put(state.players, player_name, Player.die(player_state)),
+        max_score: Score.max(state.max_score, user_score)
     }
-    #Persist score
+
+    # Persist score
     if Score.equal?(state.max_score, user_score) do
       {:ok, table} = :dets.open_file(:scoring, [])
       :dets.insert(table, {:max, [String.to_charlist(player_name), user_score.value]})
@@ -95,15 +112,13 @@ defmodule DynoGangWeb.GameServer do
   end
 
   def handle_call({:player_names}, _from, state) do
-    #get the player names
+    # get the player names
     player_names = Game.player_names(state)
     {:reply, player_names, state}
   end
 
   def handle_cast({:remove_player, player}, state) do
-    new_state = %{state |
-      players: Map.delete(state.players, player)
-    }
+    new_state = %{state | players: Map.delete(state.players, player)}
     Endpoint.broadcast!("obstacle:all", "player_left", %{name: player})
     player_names = Game.player_names(new_state)
     IO.inspect(player, label: "removed player")
@@ -111,15 +126,23 @@ defmodule DynoGangWeb.GameServer do
     {:noreply, new_state}
   end
 
-
-  def handle_info(:obstable_generator, state)  do
-    obstacle_type = :random.uniform(4)
+  def handle_info(:obstacle_generator, state) do
+    obstacle_type = :rand.uniform(4)
     min = 1500
     max = 2800
-    next_wait = floor(:random.uniform() * (max-min) + min)
-    :timer.send_after(next_wait, :obstable_generator)
+    next_wait = floor(:rand.uniform() * (max - min) + min)
     Endpoint.broadcast!("obstacle:all", "obstacle_event", %{type: obstacle_type})
+    total_players = Game.total_players(state)
+
+    obstacle_generator_ref = nil
+
+    if Game.total_players(state) > 0 do
+      {:ok, obstacle_generator_ref} = :timer.send_after(next_wait, :obstacle_generator)
+    else
+      IO.puts("No players found, obstable generator stopped")
+    end
+
+    state = %{state | obstacle_generator: obstacle_generator_ref}
     {:noreply, state}
   end
-
 end
